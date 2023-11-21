071a0342a5978ff71a78eb29e22db988df30f131
Fernanda Souza <fecafumee@gmail.com>
2023-11-21 15:22:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from '../src/components/Navbar';
+import Footer from '../src/components/Footer';
+import HomePage from '../src/pages/HomePage';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
