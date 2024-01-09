94743fa53b5e3f6c93e0a8c88873cb05bddd8302
Louise Carnevali <loucafumee@gmail.com>
2024-01-09 16:24:59 -0400

    added outsideClick on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Navbar.css';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -10,14 +10,34 @@ const Navbar = () => {
     setMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!event.target.closest('.navbar-container')) {
+        closeMenu();
+      }
+    };
+
+    if (isMenuOpen) {
+      document.body.addEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isMenuOpen]);
+
   const isLinkActive = (path) => {
     return location.pathname === path ? 'active' : '';
   };
 
   return (
     <div className={`navbar-container ${isMenuOpen ? 'menu-open' : ''}`}>
-      <Link to='/'>
-        <img src='/img/logo.png' alt="Joker's Burguer Logo" />
+      <Link to="/">
+        <img src="/img/logo.png" alt="Joker's Burguer Logo" />
       </Link>
 
       {/* Menu Hamburger */}
@@ -25,27 +45,27 @@ const Navbar = () => {
         className={`hamburger-menu ${isMenuOpen ? 'open' : ''}`}
         onClick={toggleMenu}
       >
-        <i className='material-symbols-outlined'>menu</i>
+        <i className="material-symbols-outlined">menu</i>
       </div>
       {/* End Menu Hamburger */}
 
       <ul className={`navbar-items ${isMenuOpen ? 'show' : ''}`}>
         <li>
-          <Link to='/menu' className={isLinkActive('/menu')}>
+          <Link to="/menu" className={isLinkActive('/menu')}>
             CARDÁPIO
           </Link>
         </li>
         <li>
-          <Link to='/contact' className={isLinkActive('/contact')}>
+          <Link to="/contact" className={isLinkActive('/contact')}>
             CONTATO
           </Link>
         </li>
         <li>
           <a
             href={`https://jokersburger.com.br/jokers_burger`}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='navbar-button'
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navbar-button"
           >
             FAÇA SEU PEDIDO
           </a>
